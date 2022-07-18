e03ecb65161d6e63c24d6705fc5b05ccddc0aa91
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-18 12:11:16 -0700

    Initial Commit

---
+++ Modals-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Modals-SwiftUI
+//
+//  Created by Mitya Kim on 7/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Modals-SwiftUI/Modals_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Modals_SwiftUIApp.swift
+//  Modals-SwiftUI
+//
+//  Created by Mitya Kim on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Modals_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
